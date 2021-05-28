65bf55f21340eb3ebf43779f97b0fd051c4638fa
손창대 <changdae20@naver.com>
2021-05-29 05:33:34 +0900

    도움말 추가 및 indent 수정

--- index.js
+++ index.js
@@ -7,19 +7,22 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === '!ㅇ') {
-    msg.delete();
-    msg.channel.send('나도그렇게생각해');
-  }
-  if (msg.content === '!ㄴ') {
-    msg.delete();
-    msg.channel.send('나는그렇게생각하지않아');
-  }
-  if (msg.content === '!자라') {
-    msg.delete();
-    msg.channel.send('자라');
-  }
-  if (msg.content === "!오늘의 운세"){
+    if (msg.content === '!help' || msg.content === '!도움말') {
+        msg.channel.send('현재 사용가능한 명령어입니다 : \n\n!ㅇ : 나도그렇게생각해\n!ㄴ : 나는그렇게생각하지않아\n!자라 : 자라\n!오늘의 운세 : 야근?칼퇴?연차?');
+    }
+    if (msg.content === '!ㅇ') {
+        msg.delete();
+        msg.channel.send('나도그렇게생각해');
+    }
+    if (msg.content === '!ㄴ') {
+        msg.delete();
+        msg.channel.send('나는그렇게생각하지않아');
+    }
+    if (msg.content === '!자라') {
+        msg.delete();
+        msg.channel.send('자라');
+    }
+    if (msg.content === "!오늘의 운세"){
         var value = Math.random();
         if(value < 0.65)
             msg.channel.send("슬프네요.. 오늘은 실험결과가 좋지않아 야근을 해야할 것 같아요..");
@@ -29,7 +32,7 @@ client.on('message', msg => {
             msg.channel.send("오늘의 대박! 오늘은 연차를 내서 집에서 쉴 수 있어요.");
         else
             msg.channel.send("오늘 드디어 퀠을 하드클 했는데 B클이네요...");
-  }
+    }
 });
 
 client.login(token.token);
