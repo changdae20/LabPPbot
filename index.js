c7e19720676e8236033d59e3e7f48e223d73cd9c
Ubuntu <ubuntu@ip-172-31-46-7.ap-northeast-2.compute.internal>
2021-06-03 02:03:46 +0000

    add fortune text, add cat image function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const token = require("./token.json");
 
 function check_sleep(text){
-    let corpus = ["졸려","잠와","피곤해","피곤행","졸령","잘까","일어남","기상","자야지","자기","졸리다","피곤하","자야징","잘까","아프다","자려고","잘라고","졸리네","자야하","자러감","개피곤","잔다","자야하나","자야겠다","힘들다","힘드네","힘드넹","어지럽","아프네","아프넹","아파","자러간다","자러가기"];
+    let corpus = ["졸려","잠와","피곤해","피곤행","졸령","잘까","일어남","기상","자야지","자기","졸리다","피곤하","자야징","잘까","아프다","자려고","잘라고","졸리네","자야하","자러감","개피곤","잔다","자야하나","자야겠다","힘들다","힘드네","힘드넹","어지럽","아프네","아프넹","아파","자러간다","자러가기","잠온다","자고싶다","자야할까","졸립다","힘들엉","힘드렁"];
     for(let i=0; i<corpus.length;i++){
         if(text.indexOf(corpus[i])!=-1) return true;
     }
@@ -15,6 +15,11 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    if (msg.content === "!단또"){
+        let value = parseInt(Math.random()*110)+1;
+        const attachment = new Discord.MessageAttachment("http://changdae20.synology.me:7080/cat/"+value+".JPG");
+        msg.channel.send(attachment);
+    }
     if (msg.content === '!help' || msg.content === '!도움말') {
         msg.channel.send('현재 사용가능한 명령어입니다 : \n\n!ㅇ : 나도그렇게생각해\n!ㄴ : 나는그렇게생각하지않아\n!자라 : 자라\n!오늘의 운세 : 야근?칼퇴?연차?');
     }
@@ -27,15 +32,46 @@ client.on('message', msg => {
         msg.channel.send('나는그렇게생각하지않아');
     }
     if (msg.content === "!오늘의 운세"){
-        var value = Math.random();
-        if(value < 0.65)
-            msg.channel.send("슬프네요.. 오늘은 실험결과가 좋지않아 야근을 해야할 것 같아요..");
-        else if(value < 0.85)
-            msg.channel.send("와! 오늘은 교수님과 박사님이 안계셔요! 칼퇴할 수 있어요.");
-        else if(value < 0.99)
-            msg.channel.send("오늘의 대박! 오늘은 연차를 내서 집에서 쉴 수 있어요.");
-        else
-            msg.channel.send("오늘 드디어 퀠을 하드클 했는데 B클이네요...");
+        if(msg.member.user.username=="랩경원" || msg.member.user.username=="니코는고양이"){
+            var value = Math.random();
+            if(value < 0.2)
+                msg.channel.send("오늘은 느낌이 좋아요! 뭐든지 할 수 있을 것 같은날이에요!");
+            else if(value < 0.4)
+                msg.channel.send("저녁을 먹기 전에 퇴근을 할 수 있을 것 같아요! 리겜을 하러 가도 괜찮은 컨디션이에요!");
+            else if(value < 0.6)
+                msg.channel.send("오늘은 아무도 나를 건드릴 수 없어요. 하고 싶은걸 하는 날이에요!");
+            else if(value < 0.8)
+                msg.channel.send("날씨도 좋아서 느낌도 좋아요! 오늘은 산책을 가고 싶은 날이에요!");
+            else if(value < 0.9)
+                msg.channel.send("오늘은 뭔가 낌새가 이상해요. 뭘 하든 조심히 하는게 좋을 것 같아요.");
+            else
+                msg.channel.send("슬프네요.. 오늘은 실험결과가 좋지않아 야근을 해야할 것 같아요..");
+        }else{
+            var value = Math.random();
+            if(value < 0.45)
+                msg.channel.send("슬프네요.. 오늘은 실험결과가 좋지않아 야근을 해야할 것 같아요..");
+            else if(value < 0.65)
+                msg.channel.send("와! 오늘은 교수님과 박사님이 안계셔요! 칼퇴할 수 있어요.");
+            else if(value < 0.79)
+                msg.channel.send("오늘의 대박! 오늘은 연차를 내서 집에서 쉴 수 있어요.");
+            else if(value < 0.80)
+                msg.channel.send("오늘 드디어 퀠을 하드클 했는데 B클이네요...");
+            else{
+                var value2 = Math.random();
+                if(value2 < 0.2)
+                    msg.channel.send("오늘은 느낌이 좋아요! 뭐든지 할 수 있을 것 같은날이에요!");
+                else if(value2 < 0.4)
+                    msg.channel.send("저녁을 먹기 전에 퇴근을 할 수 있을 것 같아요! 리겜을 하러 가도 괜찮은 컨디션이에요!");
+                else if(value2 < 0.6)
+                    msg.channel.send("오늘은 아무도 나를 건드릴 수 없어요. 하고 싶은걸 하는 날이에요!");
+                else if(value2 < 0.8)
+                    msg.channel.send("날씨도 좋아서 느낌도 좋아요! 오늘은 산책을 가고 싶은 날이에요!");
+                else if(value2 < 0.9)
+                    msg.channel.send("오늘은 뭔가 낌새가 이상해요. 뭘 하든 조심히 하는게 좋을 것 같아요.");
+                else
+                    msg.channel.send("슬프네요.. 오늘은 실험결과가 좋지않아 야근을 해야할 것 같아요..");
+            }
+        }
     }
     if (msg.content === '!자라' ) {
         msg.delete();
@@ -44,7 +80,7 @@ client.on('message', msg => {
     if(check_sleep(msg.content)){
         setTimeout(function(){
             msg.channel.send('자라');
-        },3000);
+        },2000);
     }
     if(msg.content.substring(0,5)==="!say "){
         if(msg.member.user.username == "관성" && msg.member.user.discriminator=="7730"){
